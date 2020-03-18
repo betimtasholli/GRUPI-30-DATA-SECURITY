731e19cc2c2a0e2b642e5117ebfc88a9ef21ca19
albin433 <albin_se@outlook.com>
2020-03-17 19:34:32 -0700

    tapcode2

--- Project9/Source.cpp
+++ Project9/Source.cpp
@@ -98,7 +98,7 @@ void morseCodeText() {
 
 
 void encryptToTapCode() {
-	const char shkronjat[26] = { ' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'l',
+	const char karakteret[26] = { ' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'l',
 							 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
 
 	const string tapCode[26] = { "/", "._.", "._..","._...", "._....", "._.....",
@@ -106,29 +106,29 @@ void encryptToTapCode() {
 									  "..._.", "..._..", "..._...", "..._....", "..._.....",
 									  "...._.", "...._..","...._...", "...._....", "...._.....",
 									  "....._.", "....._..", "....._...", "....._....", "....._....." };
-	string textToChange = "";
-	string newText = "";
+	string	givenText = "";
+	string encryptedText = "";
 
 	cout << "Shkruani Textin qe doni ta shendrroni ne Tap Code: ";
-	getline(cin, textToChange);
-	transform(textToChange.begin(), textToChange.end(), textToChange.begin(), ::tolower);
+	getline(cin, givenText);
+	transform(givenText.begin(), givenText.end(), givenText.begin(), ::tolower);
 	// ne kete rast funksioni transform i shenderron shkronjat e medha ne shkronja te vogla
 
-	for (unsigned int i = 0; i < textToChange.size(); i++) {
+	for (unsigned int i = 0; i < givenText.size(); i++) {
 		for (unsigned short j = 0; j < 26; j++) {
-			if (textToChange[i] == shkronjat[j]) {
-				newText += tapCode[j];
-				newText += "   ";
+			if (givenText[i] == karakteret[j]) {
+				encryptedText += tapCode[j];
+				encryptedText += "   ";
 				break;
 			}
 		}
 	}
 
-	cout << "Mesazhi i enkriptuar: " << newText;
+	cout << "Mesazhi i enkriptuar: " << encryptedText;
 }
 
 void decryptFromTapCode() {
-	const char shkronjat[26] = { ' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'l',
+	const char karakteret[26] = { ' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'l',
 								 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
 
 	const string tapCode[26] = { "/", "._.", "._..","._...", "._....", "._.....",
@@ -137,31 +137,31 @@ void decryptFromTapCode() {
 									  "...._.", "...._..","...._...", "...._....", "...._.....",
 									  "....._.", "....._..", "....._...", "....._....", "....._....." };
 
-	int nrChar;
+	int nr;
 	cout << "Sa karaktere do kete teksti qe do ta ktheni ne tap code: ";
-	cin >> nrChar;
+	cin >> nr;
 	cout << endl;
 
-	string textToChange[150];
-	string newText = "";
+	string givenText[150];
+	string decryptedText = "";
 
 	cout << "Shkruani Morse Code-in qe doni ta shendrroni ne Text: " << endl;
-	for (int i = 0; i < nrChar; i++) {
+	for (int i = 0; i < nr; i++) {
 		cout << "Kodi mors i shkronjes numer " << i + 1 << ": ";
-		cin >> textToChange[i];
+		cin >> givenText[i];
 	}
 
-	for (unsigned int i = 0; i < nrChar; i++) {
+	for (unsigned int i = 0; i < nr; i++) {
 		for (unsigned short j = 0; j < 37; j++) {
-			if (textToChange[i] == tapCode[j]) {
-				newText += shkronjat[j];
-				newText += "";
+			if (givenText[i] == tapCode[j]) {
+				decryptedText += karakteret[j];
+				decryptedText += "";
 				break;
 			}
 		}
 	}
 
-	cout << endl << "Mesazhi i dekriptuar: " << newText;
+	cout << endl << "Mesazhi i dekriptuar: " << decryptedText;
 }
 
 void tapCodeText() {
