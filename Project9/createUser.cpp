a71e11a743fca27c90946341622ebb230e457ecc
Egzon Gashi <59171322+egzongashi00@users.noreply.github.com>
2020-04-21 20:40:25 +0200

    Update createUser.cpp

--- Project9/createUser.cpp
+++ Project9/createUser.cpp
@@ -8,7 +8,7 @@ void createUser() {
     char characters[37] = { '_','0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z' };
 
 	string name;
-	cout << "$ds create user ";
+	cout << endl << "$ds create user ";
 	getline(cin, name);
     transform(name.begin(), name.end(), name.begin(), ::toupper);
 
